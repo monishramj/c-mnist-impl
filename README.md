efb65ddb3657a041678d65afa705b02f6e8f9379
monishramj <monishramj621@gmail.com>
2026-05-12 15:47:41 -0500

    implemented base matrix manipulation + prng

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ml Library Implementation in C
---
+++ prng.h
@@ -0,0 +1,16 @@
+// https://www.youtube.com/watch?v=_pDPfFsG89A
+
+typedef struct
+{
+  u64 state;
+  u64 inc;
+} prng_state;
+
+void prng_seed_r(prng_state *rng, u64 initstate, u64 initseq);
+void prng_seed(u64 initstate, u64 initseq);
+
+u32 prng_rand_r(prng_state *rng);
+u32 prng_rand(void);
+
+f32 prng_randf_r(prng_state *rng);
+f32 prng_randf(void);
